9bd1906ad44359f383ecfdfab3d40dcceacf36cc
luduca2109-sudo <luduca2109@hotmail.com>
2026-05-30 16:03:41 -0500

    Dev (#8)
    * prueba de YML para integración continua manual
    
    * prueba para CI/CD con codepipeline y s3
    
    * cambios para pruebas automatizadas
    
    * correccion nombre archivo pruebas
    
    * de nuevo cambio en el archivo prueba
    
    * Correcciones definitivas a pruebas automatizadas

---
+++ src/assets/app.test.tsx
@@ -0,0 +1,7 @@
+import { describe, it, expect } from 'vitest';
+
+describe('Prueba de humo del Dashboard', () => {
+  it('Debería validar que la matemática básica funciona', () => {
+    expect(1 + 1).toBe(2);
+  });
+});
